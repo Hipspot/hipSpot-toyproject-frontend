e62f8d80656a4c8490ebf5171ad3c18b62315a4e
yeoularu <yeoularu@gmail.com>
2022-10-17 15:58:42 +0900

    remove <React.StrictMode>

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ if (process.env.NODE_ENV === "development") {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <App />
-      </React.Suspense>
-    </RecoilRoot>
-  </React.StrictMode>
+  <RecoilRoot>
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </React.Suspense>
+  </RecoilRoot>
 );
