6e7886d65c619f84761bec14135800cdfba2f792
yeoularu <yeoularu@gmail.com>
2022-10-17 16:09:05 +0900

    response 404 error code if mock data not found

--- src/mocks/todo/service.js
+++ src/mocks/todo/service.js
@@ -22,7 +22,7 @@ export const editTodo = (req, res, ctx) => {
   if (!req.body.hasOwnProperty("id")) return;
   const id = parseInt(req.params.id);
   const target = data.find((todo) => todo.id === id);
-  if (!target) return;
+  if (!target) return res(ctx.status(404));
   target.title = req.body.title;
   target.content = req.body.content;
   target.tag = req.body.tag;
@@ -33,7 +33,7 @@ export const toggleTodo = (req, res, ctx) => {
   if (req.body.hasOwnProperty("id")) return;
   const id = parseInt(req.params.id);
   const target = data.find((todo) => todo.id === id);
-  if (!target) return;
+  if (!target) return res(ctx.status(404));
   if (target.status === "todo") target.status = "done";
   else target.status = "todo";
   return res(ctx.status(200), ctx.json(data));
@@ -42,7 +42,7 @@ export const toggleTodo = (req, res, ctx) => {
 export const deleteTodo = (req, res, ctx) => {
   const id = parseInt(req.params.id);
   const targetIndex = data.findIndex((todo) => todo.id === id);
-  if (targetIndex === -1) return;
+  if (targetIndex === -1) return res(ctx.status(404));
   data.splice(targetIndex, 1);
   return res(ctx.status(200), ctx.json(data));
 };
